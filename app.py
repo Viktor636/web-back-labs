b9f5f2ce2024653133a499643b5f1185fbf614ec
Виктор Гайдабура <saxarer@mail.ru>
2025-09-16 15:19:09 +0700

    Стили

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def web():
         </html>""", 200, {
             'X-Server': 'sample',
             'Content-Type': 'text/plain; charset=utf-8'
-            }
+           }
 
 @app.route("/author")
 def author():
@@ -41,8 +41,12 @@ def author():
 @app.route("/image")
 def image():
     path = url_for("static", filename="DYB.jpg")
+    css_path = url_for("static", filename="lab1.css")
     return '''<!doctype html> 
         <html> 
+            <head>
+                <link rel="stylesheet" href="''' + css_path + '''">
+            </head>
             <body> 
                 <h1>Дуб</h1>
                 <img src="''' + path + '''">

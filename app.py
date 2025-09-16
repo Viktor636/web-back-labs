8b738a9f187890896d25faacab3e8612639ed8fb
Виктор Гайдабура <saxarer@mail.ru>
2025-09-16 15:34:48 +0700

    Меню первой лабораторной

--- app.py
+++ app.py
@@ -25,6 +25,32 @@ def index():
             </body>
         </html>"""
 
+@app.route("/lab1")
+def lab1():
+    return """<!doctype html>
+        <html>
+            <head>
+                <title>Лабораторная 1</title>
+            </head>
+            <body>
+                <p>Flask — фреймворк для создания веб-приложений на языке
+                программирования Python, использующий набор инструментов
+                Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+                называемых микрофреймворков — минималистичных каркасов
+                веб-приложений, сознательно предоставляющих лишь самые базовые возможности.</p>
+                <a href="/">Корень сайта</a>
+                
+                <h2>Список роутов</h2>
+                <ul>
+                    <li><a href="/lab1/web">Web</a></li>
+                    <li><a href="/lab1/author">Author</a></li>
+                    <li><a href="/lab1/image">Image</a></li>
+                    <li><a href="/lab1/counter">Counter</a></li>
+                    <li><a href="/lab1/info">Info</a></li>
+                </ul>
+            </body>
+        </html>"""
+
 @app.route("/lab1/web")
 def web():
     return """<!doctype html> 

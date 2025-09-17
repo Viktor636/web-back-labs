b1e75d779b5fec10262d21f7fd0c479bfe81960e
Виктор Гайдабура <saxarer@mail.ru>
2025-09-17 18:24:36 +0700

    Имя студента

--- app.py
+++ app.py
@@ -268,4 +268,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Виктор Гайдабура'
+    return render_template('example.html', name=name)

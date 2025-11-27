316c3941e2552413040786b8582ef31dfb90b59b
Виктор Гайдабура <saxarer@mail.ru>
2025-11-27 09:50:21 +0700

    Проверка описания

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -55,6 +55,8 @@ function deleteFilm(id, title) {
 
 function showModal() {
     document.querySelector('div.modal').style.display = 'block';
+    // Очистка сообщения об ошибке при открытии модального окна
+    document.getElementById('description-error').innerText = '';
 }
 function hideModal() {
     document.querySelector('div.modal').style.display = 'none';
@@ -90,9 +92,17 @@ function sendFilm() {
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(film)
     })
-    .then(function() {
-        fillFilmList();
-        hideModal();
+    .then(function(resp) {
+        if(resp.ok) {
+            fillFilmList();
+            hideModal();
+            return {};
+        }
+        return resp.json();
+    })
+    .then(function(errors) {
+        if(errors.description)
+            document.getElementById('description-error').innerText = errors.description;
     });
 }
 

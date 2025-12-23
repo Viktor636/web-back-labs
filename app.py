91af99ce55243f776d64f0e25150892365b91a00
Виктор Гайдабура <saxarer@mail.ru>
2025-12-23 21:15:05 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 app = Flask(__name__)
 
@@ -54,6 +55,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 
 @app.route("/")
@@ -89,6 +91,9 @@ def index():
                 </ul>
                 <ul>
                     <li><a href="/lab8/">Лабораторная работа 8</a></li>
+                </ul>
+                <ul>
+                    <li><a href="/lab9/">Лабораторная работа 9</a></li>
                 </ul>
                 <footer>
                     Гайдабура Виктор Васильевич, ФБИ-31, 3 курс, 2025
